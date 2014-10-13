3ad6ad1d5b0b0517f36e5da88acc115620f14c1b
Steve Harris <doc@steveharris.me>
2014-10-13 15:34:18 +0100

    added: initial quick look table 1 by different factors

---
+++ prep_data.R
@@ -0,0 +1,163 @@
+# author: Steve Harris
+# date: 2014-10-09
+# subject: Prepare data for analysis
+
+# Readme
+# ======
+
+# Assuming ..data/_data_in/6 centres-15-09-14.xls is the most current file
+# Converted this to CSV by hand
+
+# Todo
+# ====
+
+
+# Log
+# ===
+# 2014-10-09
+# - file created
+# - data dictionary converted to plain text
+# - data sheet in ..data/_data_in/6 centres-15-09-14.xls saved as CSV file
+# 2014-10-13
+# - cloned from labbook_141013.R
+
+library(Hmisc)
+library(assertthat)
+library(gmodels)
+library(data.table)
+
+# Assuming ..data/_data_in/6 centres-15-09-14.xls is the most current file
+warning("Check:\nAssuming ..data/_data_in/6 centres-15-09-14.xls is the most current file")
+raw_csv_file = '../data/working_140915.csv'
+rdf <- read.table(raw_csv_file,
+	sep = ",", quote="\"", header=TRUE,
+	strip.white=TRUE, stringsAsFactors=FALSE )
+str(rdf)
+
+# Drop if missing id (there's a blank row at the end of the sheet)
+tail(data.frame(rdf$id, rdf$age))
+nrow(rdf)
+rdf <- rdf[rdf$id != "",]
+nrow(rdf)
+assert_that(nrow(rdf) == 596)
+
+# Sanity checks
+df.names <- data.frame(names(rdf))
+length(df.names[,])
+df.names
+
+with(rdf, head(data.frame(num, id, age, gender)))
+
+# Prepare working data frame
+# Only add variables as you need them
+
+# Check that ID is unique
+assert_that(sum(duplicated(rdf$id)) == 0)
+
+# Prepare empty data frame
+rm(wdt)
+wdt <- data.table(id.original = rdf$id)
+wdt$id <- c(1:nrow(wdt))
+head(wdt)
+
+
+# Now start adding variables
+wdt$hosp <- tolower(gsub("([a-z]?)-[0-9]+","\\1", wdt$id.original))
+factor(wdt$hosp)
+table(wdt$hosp)
+describe(wdt$hosp)
+assert_that(
+	nrow(wdt)
+	- sum(wdt$hosp %in% c('an', 'rome', 'blf', 'lee', 'rlh', 'uclh'))
+	== 0
+	)
+
+# Patient characteristics
+wdt$male 	<- ifelse(rdf$gender   == 1, 1, 0)
+describe(wdt$male)
+
+wdt$age <- rdf$age
+describe(wdt$age)
+
+describe(rdf$height)
+wdt$height <- rdf$height
+
+describe(rdf$weight)
+wdt$weight <- rdf$weight
+
+describe(rdf$bmi)
+wdt$bmi <- rdf$bmi
+
+# Sepsis
+describe(rdf$source_infection)
+wdt$sepsis.site <- rdf$source_infection
+factor(wdt$sepsis.site)
+wdt[,list(.N, pct=.N/nrow(wdt)*100),sepsis.site]
+
+# TODO: 2014-10-13 - [ ] decide which bugs you want reported (will need cleaning)
+describe(rdf$bug1)
+describe(rdf$bug2)
+
+# Medications
+describe(rdf$b_block_hist)
+wdt$pmh.betablock <- rdf$b_block_hist
+
+# Severity
+describe(rdf$adm_sofa)
+wdt$sofa.0 <- rdf$adm_sofa
+describe(rdf$sofa_1)
+wdt$sofa.1 <- rdf$sofa_1
+
+# NOTE: 2014-10-13 - error in naming sofa_24 column name
+if ('sofa._24') %in% names(rdf) {
+	try(names(rdf)[names(rdf)=='sofa._24'] <- 'sofa_24', silent=FALSE)
+}
+describe(rdf$sofa_24)
+wdt$sofa.24 <- rdf$sofa_24
+
+# Norad, heart rate etc
+obs <- list(
+	c('ne', 'ne'),
+	c('hr', 'hr'),
+	c('map', 'map'),
+	c('syst_bp', 'bps')
+	)
+
+# names(rdf)
+# str(wdt)
+
+# TODO: 2014-10-13 - [ ] nested loop: fix
+for (i in 1:length(obs)) {
+
+	suffixes <- c(1, 24)
+	for (j in 1:length(suffixes)) {
+		suffix <- suffixes[j]
+		# print(describe(rdf[paste(obs[[i]][1], '1', sep='_')]))
+		print(paste(obs[[i]][1], j, sep='_'))
+		ob_orig <- paste(obs[[i]][1], suffix, sep='_')
+		print(str(rdf[ob_orig]))
+
+		ob_new <- paste(obs[[i]][2], suffix, sep='.')
+		wdt[,ob_new] <- rdf[ob_orig]
+		print(str(wdt[,ob_new]))
+	}
+
+}
+# str(wdt)
+
+
+# Outcomes
+describe(rdf$itu_mortality)
+wdt$mort.itu <- rdf$itu_mortality
+
+# NOTE: 2014-10-09 - missing hospital mortality for 63 patients
+describe(rdf$hosp_mortality)
+wdt$mort.hosp <- rdf$hosp_mortality
+wdt[,list(.N,mort.hosp.miss = sum(is.na(mort.hosp)) ),hosp]
+
+head(wdt)
+
+# Save
+save(wdt, file='../data/working.RData')
+
+
---
+++ table1_byNorad.R
@@ -0,0 +1,306 @@
+# author: Steve Harris
+# date: 2014-10-09
+# subject: Produce Table 1 for the norad study - by hospital
+
+# Readme
+# ======
+
+
+# Todo
+# ====
+
+# *- [X] TODO: add by strata functionality
+# *- [X] TODO: calculate percentages for categories
+# *- [X] TODO: add total missing by category
+# *- [X] TODO: total missing over strata for continuous
+# *- [X] TODO: reshape strata if they exist to wide
+# *- [X] TODO: needs to export
+
+# TODO: 2014-10-13 - [ ] convert this to 3 way comparison by norad threshold
+
+# Notes
+#
+
+# Log
+# ===
+# 2014-10-09
+# - file created
+# 2014-10-10
+# - works, produces a single data table with all the vars
+# 2014-10-11
+# - switch to using development version of data.table
+# 2014-10-11
+# - seems to be working
+# - handles strata
+# - exports complete and formatted data to excel
+# 2014-10-12
+# - rewrite of t1.catvars function which was completely wrong!
+# - fixed table ordering
+# 2014-10-13
+# - duplicated from paper-spotearly/labbook_table1.R
+# - duplicated from table1_all.R
+# - duplicated from table1_byHosp.R
+
+
+#  =====================
+#  = Load dependencies =
+#  =====================
+library(data.table) # NB setnames and setorder only exist in >v1.9
+library(reshape2)
+library(XLConnect)
+
+rm(list=ls(all=TRUE))
+load(file='../data/working.RData')
+wdt.original <- wdt
+wdt$sample_N <- 1
+
+# Define file name
+table1.file <- '../outputs/tables/table1_byNorad.xlsx'
+
+# Define strata
+# NOTE: 2014-10-13 - analyse all
+wdt$ne.1.cut	<- cut2(wdt$ne.1, c(0.1, 0.3))
+describe(wdt$ne.1.cut)
+vars.strata <-  'ne.1.cut'
+
+# Define the vars
+
+vars <- c(
+	'male', 'age', 'weight', 'height', 'bmi',
+	# 'sepsis.site', TODO: 2014-10-13 - [ ] this is not being treated as a catvar
+	'pmh.betablock', 
+	'sofa.0', 'sofa.1', 'sofa.24',
+	'ne.1', 'ne.24',
+	'hr.1', 'hr.24',
+	'map.1', 'map.24',
+	'bps.1', 'bps.24',
+	'mort.itu', 'mort.hosp'
+	)
+
+vars <- c('sample_N', vars) # prepend all obs for total counts
+
+# If no strata defined then use sample_N dummy variable
+if (is.na(vars.strata)) {
+	vars.strata <- 'sample_N'
+}
+
+# Define the characteristics of the variables
+vars.factor <- c(
+	'male',
+	# 'sepsis.site', TODO: 2014-10-13 - [ ] fix sepsis.site
+	'pmh.betablock',
+	'mort.itu', 'mort.hosp'
+	)
+
+# Define distributions
+vars.norm 	<- c('age', 'height', 'weight')
+
+# NOTE: 2014-10-12 - you need to have the strata var in the data.table
+if (!vars.strata %in% vars) {
+	vars <- c(vars, vars.strata)
+	vars.factor <- c(vars.factor, vars.strata)
+}
+vars.factor <- c('sample_N', vars.factor) # prepend all obs for total counts
+vars.cont 	<- vars[!vars %in% vars.factor]
+
+
+# Now update columns to be factors if not already
+wdt[, (vars.factor) :=lapply(.SD, as.factor), .SDcols=vars.factor]
+
+# Remove the stratifying variable from the factor vars
+vars.factor <- vars.factor[!vars.factor %in% vars.strata]
+
+# Now produce the summaries for the continuous vars
+# TODO: 2014-10-10 - add n, p.miss skew kurt, q5 q95
+# NOTE: 2014-10-11 - lapply to sapply since then returns vectors not lists
+
+t1.contvars <- function(var, strata, this_dt) {
+	this_dt[,
+		list(
+				varname	 = var,
+				N 		 = .N,
+				# NOTE: 2014-10-11 - add these later else strata means you get missing by strata not var
+				miss.n	 = sapply(.SD, function(x) sum(is.na(x))),
+				# miss.p	 = sapply(.SD, function(x) round(sum(is.na(x)) / length(x) * 100, 1)),
+				mean	 = sapply(.SD, function(x) mean(x, na.rm=TRUE)),
+			 	sd		 = sapply(.SD, function(x) sd(x, na.rm=TRUE)),
+			 	min		 = sapply(.SD, function(x) min(x, na.rm=TRUE)),
+			 	q05		 = sapply(.SD, function(x) quantile(x, 0.05, na.rm=TRUE)),
+			 	q25		 = sapply(.SD, function(x) quantile(x, 0.25, na.rm=TRUE)),
+			 	q50		 = sapply(.SD, function(x) quantile(x, 0.50, na.rm=TRUE)),
+			 	q75		 = sapply(.SD, function(x) quantile(x, 0.75, na.rm=TRUE)),
+			 	q95		 = sapply(.SD, function(x) quantile(x, 0.95, na.rm=TRUE)),
+			 	max		 = sapply(.SD, function(x) max(x, na.rm=TRUE))
+			 	),
+			by=strata,
+			.SDcols = var
+	]
+}
+# t1.contvars(c('lactate', 'bpsys'), c('room_cmp', 'sex'), wdt)
+# t1.contvars('sofa.1', vars.strata, wdt)
+
+# Now do this for all the continuous vars
+t1.contvars.results <- t1.contvars(vars.cont, vars.strata, wdt)
+
+# Now total the missing by variable
+t1.contvars.results[,miss.n := sum(miss.n), by=varname]
+t1.contvars.results[, miss.p := lapply(.SD, function(x) round(max(miss.n)/sum(N) *100, 1)), by=varname ]
+
+# Now convert this list of data.tables into a single data.table
+t1.contvars.results$vartype <- 'continuous'
+t1.contvars.results
+
+# Categorical vars
+
+t1.catvars <- function(var_as_string, strata_as_string, this_dt) {
+
+	strata 		<- this_dt[[strata_as_string]]
+	var 		<- this_dt[[var_as_string]]
+
+	var <- with(this_dt, get(var_as_string))
+	t2 <- this_dt[,.(strata.rows=.N),by=strata]
+
+	t1 <- this_dt[, .N ,by=list(var, strata)]
+	setkey(t1, strata)
+	setkey(t2, strata)
+	t1 <- t1[t2]
+
+	t1[,`:=` (
+		pct 	= round(N/strata.rows*100, 1),
+		varname = var_as_string,
+		vartype = 'categorical',
+		strata.rows = NULL
+		)]
+	setnames(t1, 'var', 'level')
+	setnames(t1, 'strata', strata_as_string)
+	return(t1)
+}
+# t1.catvars('dead28','early4', wdt)
+
+t1.catvars.results <- lapply(vars.factor, function(var) t1.catvars(var, vars.strata, wdt))
+t1.catvars.results <- do.call(rbind, t1.catvars.results)
+t1.catvars.results
+
+
+# TODO: 2014-10-11 - tidy up calculation of missing
+t1.catvars.results[, miss.n:=NULL, ]
+t1.catvars.results[is.na(level), miss.n := lapply(.SD, function(x) sum(N)), by=varname]
+t1.catvars.results[, miss.n := ifelse(is.na(miss.n), 0, miss.n) ]
+t1.catvars.results[, max(miss.n), by=varname ]
+t1.catvars.results[, miss.n := lapply(.SD, function(x) max(miss.n)), by=varname ]
+t1.catvars.results[, miss.p := lapply(.SD, function(x) round(max(miss.n)/sum(N) *100, 1)), by=varname ]
+
+t1.contvars.results.orig <- t1.contvars.results
+t1.contvars.results <- lapply(t1.contvars.results, function(x) if (is.list(x)) as.numeric(x) else x)
+t1.contvars.results <- as.data.table(t1.contvars.results)
+
+# Now merge these data.frames together
+cols_shared <- c(c('varname', 'vartype', 'N', 'miss.n', 'miss.p'), vars.strata)
+t1.results <- merge(t1.contvars.results, t1.catvars.results,
+	by=cols_shared, all=TRUE)
+setcolorder(t1.results, c(vars.strata, 'varname', 'level', 'N', 'pct', 'mean', 'sd', 'min', 'q05', 'q25', 'q50', 'q75', 'q95', 'max', 'miss.n', 'miss.p', 'vartype'))
+
+# Provide the table order
+t1.results[,table.order := NULL]
+t1.results[, table.order := which(vars == varname), by=varname]
+
+# Define the variables distribution
+t1.results[, dist := ifelse(varname %in% vars.norm, 'normal', '')]
+
+# setorder(t1.results, table.order, vars.strata)
+cols_all_order <- c(vars.strata, c('varname', 'miss.p', 'level', 'N', 'pct', 'mean', 'sd'))
+# t1.results[, .SD, .SDcols=cols_all_order]
+
+# Drop the NA levels since these are captured in the miss.n and miss.p fields
+t1.results <- t1.results[!(vartype=='categorical' & is.na(level))]
+# Convert level from factor to numeric to enable sorting
+t1.results[, level := as.numeric(levels(level))[level]]
+
+# Extract variable level results
+t1.results.byvarname <- t1.results[, list(
+	miss.n = max(miss.n),
+	miss.p = max(miss.p) )
+	, by=varname ]
+
+#  ====================
+#  = Raw data to wide =
+#  ====================
+t1.melt <- melt(t1.results, id=c(vars.strata, c('varname', 'level')))
+setnames(t1.melt, vars.strata, 'strata')
+
+# Drop empty strata from the table
+str(t1.melt)
+t1.melt[,.N,by=list(varname, level)]
+dcast.data.table(t1.melt, varname + level ~ strata + variable)
+t1.wide.raw <- dcast.data.table(t1.melt, varname + level ~ strata + variable,
+	subset = .(!is.na(strata)))
+
+t1.wide.raw[, table.order := which(vars == varname), by=varname]
+setorder(t1.wide.raw, table.order)
+t1.wide.raw[is.na(level), level := '']
+
+#  ==========================
+#  = Formatted data to wide =
+#  ==========================
+# Pick your results for the final formatted table
+# NOTE: 2014-10-11 - ifelse returns NA if you ask NA == 1 rather than the else clause
+# str(t1.results)
+t1.results.formatted <- t1.results[, list(
+	strata 	= get(vars.strata),
+	varname = varname,
+	level 	= level,
+	v.mid	= ifelse(vartype == 'categorical', N, mean),
+	v.left	= ifelse(vartype == 'categorical', pct, ifelse(dist == 'normal', sd, q25)),
+	v.right	= ifelse(vartype == 'categorical' | dist == 'normal', NA, q75),
+	miss.n	= miss.n,
+	miss.p	= miss.p,
+	N 		= N,
+	vartype = vartype,
+	dist 	= dist
+	)]
+
+
+# Now provide the formatting
+t1.results.formatted <- t1.results.formatted[,
+	`:=` (
+	v.fmt1 	= ifelse(vartype == 'categorical', sprintf("%.0f", v.mid), sprintf("%.1f", v.mid)),
+	v.fmt2 	= ifelse(vartype == 'categorical', paste('(', sprintf("%.1f", v.left), '%)', sep=''),
+				ifelse(vartype == 'continuous' & dist == 'normal',
+					paste('(', sprintf("%.1f", v.left), ')', sep=''),
+					paste('(', sprintf("%.1f", v.left), '--', sprintf("%.1f", v.right), ')', sep='')))
+		) ]
+
+# Now reshape to wide
+t1.melt <- melt(
+	t1.results.formatted[, list(strata, varname, level, v.fmt1, v.fmt2, N, miss.n, miss.p)],
+	id=c('strata', 'varname', 'level'))
+
+# Drop empty strata from the table
+t1.wide.summ <- dcast.data.table(t1.melt[(variable == 'v.fmt1' | variable == 'v.fmt2')], varname + level ~ strata + variable,
+	subset = .(!is.na(strata)))
+t1.wide.summ[, table.order := which(vars == varname), by=varname]
+
+t1.wide.summ[is.na(level), level := '']
+
+setkey(t1.wide.summ, varname)
+setkey(t1.results.byvarname, varname)
+t1.wide.summ <- t1.wide.summ[t1.results.byvarname]
+setorder(t1.wide.summ, +table.order, -level)
+
+#  =======================
+#  = Now export to excel =
+#  =======================
+wb <- loadWorkbook(table1.file, create = TRUE)
+
+sheet1 <- paste('varsBy_', vars.strata, sep='')
+removeSheet(wb, sheet1)
+createSheet(wb, name = sheet1)
+writeWorksheet(wb, t1.wide.summ, sheet1)
+
+sheet2 <- paste('varsBy_', vars.strata, '_detail', sep='')
+removeSheet(wb, sheet2)
+createSheet(wb, name = sheet2)
+writeWorksheet(wb, t1.wide.raw, sheet2)
+
+saveWorkbook(wb)
+
