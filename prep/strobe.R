fd50aea0bc01128d2cbcec1dc42505fcf4c630b6
Steve Harris (docsteveharris) <git@steveharris.me>
2016-03-17 17:18:16 +0000

    updated strobe for new definitions

--- prep/strobe.R
+++ prep/strobe.R
@@ -1,31 +1,47 @@
-# rm(list=ls(all=TRUE))
 # For debugging and interactive coding
+# rm(list=ls(all=TRUE))
 # source(file="../prep/load.R")
-# Generate working data via strobe
 # load(file='../data/cleaned.Rdata')
 
+#  =======================================
+#  = Generate working data and subgroups =
+#  =======================================
 
-wdt[, include := ifelse(age>=18 & ne.1 > 0, 1,0) ]
-wdt[, include := ifelse(is.na(include),0,include)]
-describe(wdt$include)
-wdt <- wdt[include==1]
+
+names(wdt)
+with(wdt, table(los.itu,mort.itu))
 
 describe(wdt$los.itu)
+# LOS post start of Norad (i.e. patients who start Norad in ITU)
 describe(wdt$los.ne)
+
+# Basic inclusion criteria
+# - on Norad
+# - adult
+# - in ICU for > 24hrs 
 nrow(wdt[los.itu==0])
-with(wdt[los.itu<=7], table(los.itu, mort.itu))
-nrow(wdt[los.ne==0])
-with(wdt[los.ne<=7], table(los.ne, mort.itu))
-wdt[, exclude.los.itu.0 := ifelse(los.itu==0 | los.ne==0,1,0)]
-wdt[, exclude.los.itu.0 := ifelse(is.na(exclude.los.itu.0),0,exclude.los.itu.0)]
-describe(wdt$exclude.los.itu.0)
+nrow(wdt[los.ne==0]) # but lot's of missingness
+wdt[, include := ifelse(age>=18 & ne.1 > 0 &
+	los.itu > 0 & (los.ne != 0 | is.na(los.ne))  , 1, 0) ]
+wdt[, include := ifelse(is.na(include),0,include)]
+describe(wdt$include)
 
 wdt[, exclude.rx.betablock := ifelse(rx.betablock==FALSE | is.na(rx.betablock),0,1)]
-
-describe(wdt$ne.24)
-nrow(wdt[ne.24==0])
 wdt[, exclude.ne.24 := ifelse(ne.24 > 0 & !is.na(ne.24),0,1)]
+wdt[, exclude.hr.24 := ifelse(hr.24 >= 95 & !is.na(hr.24),0,1)]
 
+nrow(wdt)
+nrow(wdt[include==1])
+nrow(wdt[exclude.rx.betablock==1])
+nrow(wdt[exclude.ne.24==1])
+nrow(wdt[exclude.hr.24==1])
+
+wdt[, grp.all := 1]
+wdt[, grp.ne24 := ifelse(include==1 & exclude.rx.betablock == 0 & exclude.ne.24 == 0, 1, 0)]
+wdt[, grp.hr24 := ifelse(include==1 & exclude.rx.betablock == 0 & exclude.ne.24 == 0 & exclude.hr.24 == 0, 1, 0)]
+
+# Prepare STROBE data
+wdt <- wdt[include==1]
 
 # Encode the hospitals
 (dt.hosp <- data.table(hosp=unique(wdt$hosp), hosp.id=rnorm(8)))
@@ -37,19 +53,14 @@ setkey(wdt, hosp)
 wdt <- dt.hosp[wdt]
 wdt[, hosp.id:=factor(hosp.id)]
 setkey(wdt,id)
-str(wdt)
-
-# Prepare STROBE data
-tdt <- wdt[include==1 & exclude.los.itu.0==0 & exclude.rx.betablock==0 & exclude.ne.24 ==0]
-# names(tdt)
 
 # Prepare hospital name sorted by mean mortality
 # do this after selection
-table(tdt$hosp.id)
-m <- tdt[, .(s = mean(mort.itu, na.rm=TRUE)), by=hosp.id]
+table(wdt$hosp.id)
+m <- wdt[, .(s = mean(mort.itu, na.rm=TRUE)), by=hosp.id]
 setorder(m, s)
 m[, hosp.id.sort := .I ]
 m[, hosp.id.sort := factor(hosp.id.sort, labels=m$hosp.id, ordered=TRUE)]
-setkey(tdt,hosp.id)
-tdt <- tdt[m[,.(hosp.id,hosp.id.sort)]]
-# str(tdt)
+setkey(wdt,hosp.id)
+wdt <- wdt[m[,.(hosp.id,hosp.id.sort)]]
+# str(wdt)
