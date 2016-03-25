7628cc3cf6d000e4ac7e71ee802fd9c72cbb7ac0
Steve Harris (docsteveharris) <git@steveharris.me>
2016-03-25 23:32:18 +0000

    updated treatment variable (process measure) plots

--- analysis/fig_process_var.R
+++ analysis/fig_process_var.R
@@ -4,113 +4,76 @@
 
 # Readme
 # ======
+# All at 24h
 
 
 # Todo
 # ====
-# - [ ] heart rate
-# - [ ] blood pressure
 # - [ ] norad dose
-# - [ ] first 24 h fluids
-# - [ ] sedation
-# - [ ] steroids
-# - [ ] vadi
 
+# - continuous by box-whisker
+#     - MAP
+#     - fb
+# - categorical by mosaic
+# suggest dichotomise first
+#     - sedation 
+#     - vadi
+#     - steroids
+#     - mv
+#     - rrt
 
 # Log
 # ===
 # 2016-03-09
 # - file created
 
+rm(list=ls(all=TRUE))
+
 require(Hmisc)
 require(data.table)
 require(ggplot2)
+require(ggthemes)
 
-rm(list=ls(all=TRUE))
-load(file='../data/cleaned.RData')
-
+setwd("/Users/steve/aor/academic/collab-norad/paper-euroepi/src/analysis")
 # Generate working data
-source("../prep/prep_vars.R")
 source("../prep/strobe.R")
+source("../prep/prep_vars.R")
+source("../share/functions4paper.R")
 # this saves the data to tdt so rename
-wdt <- tdt
-str(wdt)
 
 str(wdt$hosp.id.sort)
-
-# - [ ] NOTE(2016-03-15): dropping this b/c not a treatment variable
-# Heart rate
-# p <- ggplot(data=wdt, aes(y=hr.24, x=hosp.id.sort))
-# p <- p + geom_boxplot(notch=TRUE, varwidth=TRUE) +
-# 	coord_cartesian(ylim=c(40,160)) +
-# 	ylab("Heart rate (per min)") +
-# 	xlab("Intensive Care Unit") +
-# 	theme_minimal()
-# p
-# ggsave(p, file="../write/figures/process_hr.pdf", width=3, height=3, scale=2)
+wdt[,.(mort=mean(mort.itu),.N),by=.(hosp.id.sort, hosp)]
 
 # MAP
 p <- ggplot(data=wdt, aes(y=map.24, x=hosp.id.sort))
-p <- p + geom_boxplot(notch=TRUE, varwidth=TRUE) +
-	coord_cartesian(ylim=c(40,100)) +
-	ylab("Mean arterial pressure (mmHg)") +
-	xlab("Intensive Care Unit") +
-	theme_minimal()
+p <- p + geom_tufteboxplot(median.type="line", whisker.type="line", hoffset=0, width=5) +
+    coord_cartesian(ylim=c(40,100)) +
+    ylab("Mean arterial pressure (mmHg)") +
+    xlab("Intensive Care Unit") +
+    theme_pander()
 p
-ggsave(p, file="../write/figures/process_map.eps", width=3, height=3, scale=2)
+ggsave(p, file="../write/figures/process_map.pdf", width=3, height=3, scale=2)
 
 # Noradrenaline
 p <- ggplot(data=wdt, aes(y=ne.24, x=hosp.id.sort))
-p <- p + geom_boxplot(notch=TRUE, varwidth=TRUE) +
-	coord_cartesian(ylim=c(0,2)) +
+p <- p + geom_tufteboxplot(median.type="line", whisker.type="line", hoffset=0, width=5) +
+	coord_cartesian(ylim=c(0,1.5)) +
 	ylab("Noradrenaline dose (mcg/kg/min)") +
 	xlab("Intensive Care Unit") +
-	theme_minimal()
+    theme_pander()
 p
-ggsave(p, file="../write/figures/process_ne24.eps", width=3, height=3, scale=2)
+ggsave(p, file="../write/figures/process_ne24.pdf", width=3, height=3, scale=2)
 
 # Fluids
-p <- ggplot(data=wdt, aes(y=fb.24, x=hosp.id.sort))
-p <- p + geom_boxplot(notch=TRUE, varwidth=TRUE) +
-	coord_cartesian(ylim=c(-2500,12500)) +
-	ylab("Fluid balance (mls)") +
+p <- ggplot(data=wdt, aes(y=fb.24/1000, x=hosp.id.sort))
+p <- p + geom_tufteboxplot(median.type="line", whisker.type="line", hoffset=0, width=5) +
+	coord_cartesian(ylim=c(-2.5,12.5)) +
+	ylab("Fluid balance (litres)") +
 	xlab("Intensive Care Unit") +
-	theme_minimal()
-p
-ggsave(p, file="../write/figures/process_fb24.eps", width=3, height=3, scale=2)
-
-# # Sedation
-# p <- ggplot(data=wdt, aes(y=sedation.24, x=hosp.id.sort))
-# p + geom_boxplot(notch=TRUE) +
-# 	coord_cartesian(ylim=c(0,10)) +
-# 	theme_minimal()
-	
-
-# Mosaic plot with ggplot function
-ggMMplot <- function(var1, var2, ylab=""){
-  require(ggplot2)
-  levVar1 <- length(levels(var1))
-  levVar2 <- length(levels(var2))
-
-  jointTable <- prop.table(table(var1, var2))
-  plotData <- as.data.frame(jointTable)
-  plotData$marginVar1 <- prop.table(table(var1))
-  plotData$var2Height <- plotData$Freq / plotData$marginVar1
-  plotData$var1Center <- c(0, cumsum(plotData$marginVar1)[1:levVar1 -1]) +
-    plotData$marginVar1 / 2
-
-  ggplot(plotData, aes(var1Center, var2Height)) +
-    geom_bar(stat = "identity", aes(width = marginVar1, fill = var2), col = "White") +
-    scale_fill_grey(start=0,end=0.9) +
-    ylab(ylab) +
-    xlab("Intensive Care Unit") +
-    scale_y_continuous(labels = scales::percent) +
-    scale_x_continuous(labels=NULL) +
-    geom_text(aes(label = as.character(var1), x = var1Center, y = -0.05, size=0.5)) +
-    guides(fill="none", size="none") +
-    coord_fixed(ratio=1) +
     theme_pander()
-}
+p
+ggsave(p, file="../write/figures/process_fb24.pdf", width=3, height=3, scale=2)
+
 
 # Sedation
 summary(wdt$sedation.24)
@@ -119,8 +82,9 @@ tdt <- na.omit(
                hosp.id.sort,
             sedation.24.logical = ifelse(sedation.24 > 0, 1, 0)
             )])
-p <- ggMMplot(tdt$hosp.id.sort, !tdt$sedation.24.logical, ylab="Sedation")
+p <- ggMMplot(tdt$hosp.id.sort, !tdt$sedation.24.logical, xlab="Intensive Care Unit", ylab="Sedation")
 p
+ggsave(p, file="../write/figures/process_sedation.pdf", width=3, height=3, scale=2)
 
 
 # VADI
@@ -138,8 +102,9 @@ tdt <- na.omit(
                     ifelse(ne.24 > 0.1, vadi.24 - 2,
                     ifelse(ne.24 > 0.0, vadi.24 - 1, vadi.24))))])
 tdt[, vadi.other.24.logical := ifelse(vadi.other.24 > 0, 1, 0)]
-p <- ggMMplot(tdt$hosp.id.sort, tdt$vadi.other.24.logical, ylab="Other vasopressors/inotropes")
+p <- ggMMplot(tdt$hosp.id.sort, tdt$vadi.other.24.logical, xlab="Intensive Care Unit", ylab="Other vasopressors/inotropes")
 p
+ggsave(p, file="../write/figures/process_vadi.pdf", width=3, height=3, scale=2)
 
 # Steroids
 summary(wdt$rx.roids)
@@ -148,8 +113,9 @@ tdt <- na.omit(
                 hosp.id.sort,
                 rx.roids.logical = ifelse(rx.roids==TRUE, 1, 0)
             )])
-p <- ggMMplot(tdt$hosp.id.sort, tdt$rx.roids.logical, ylab="Corticosteroids")
+p <- ggMMplot(tdt$hosp.id.sort, tdt$rx.roids.logical, xlab="Intensive Care Unit", ylab="Corticosteroids")
 p
+ggsave(p, file="../write/figures/process_roids.pdf", width=3, height=3, scale=2)
 
 # Mechanical ventilation
 summary(wdt$mv.24)
@@ -158,8 +124,9 @@ tdt <- na.omit(
                 hosp.id.sort,
                 mv.24
             )])
-p <- ggMMplot(tdt$hosp.id.sort, !tdt$mv.24, ylab="Mechanical ventilation")
+p <- ggMMplot(tdt$hosp.id.sort, !tdt$mv.24, xlab="Intensive Care Unit", ylab="Mechanical ventilation")
 p
+ggsave(p, file="../write/figures/process_mv.pdf", width=3, height=3, scale=2)
 
 # Renal replacement therapy
 summary(wdt$rrt.24)
@@ -168,5 +135,6 @@ tdt <- na.omit(
                 hosp.id.sort,
                 rrt.24
             )])
-p <- ggMMplot(tdt$hosp.id.sort, !tdt$rrt.24, ylab="Renal replacement therapy")
+p <- ggMMplot(tdt$hosp.id.sort, !tdt$rrt.24, xlab="Intensive Care Unit", ylab="Renal replacement therapy")
 p
+ggsave(p, file="../write/figures/process_rrt.pdf", width=3, height=3, scale=2)
--- share/functions4paper.R
+++ share/functions4paper.R
@@ -1,4 +1,55 @@
 # Load functions
+
+# Mosaic plot with ggplot function
+# Mosaic plot with ggplot function
+ggMMplot <- function(var1, var2, xlab="", ylab=""){
+  require(ggplot2)
+  require(ggthemes)
+  levVar1 <- length(levels(var1))
+  levVar2 <- length(levels(var2))
+
+  jointTable <- prop.table(table(var1, var2))
+  plotData <- as.data.frame(jointTable)
+  plotData$marginVar1 <- prop.table(table(var1))
+  plotData$var2Height <- plotData$Freq / plotData$marginVar1
+  plotData$var1Center <- c(0, cumsum(plotData$marginVar1)[1:levVar1 -1]) +
+    plotData$marginVar1 / 2
+
+  ggplot(plotData, aes(var1Center, var2Height)) +
+    geom_bar(stat = "identity", aes(width = marginVar1, fill = var2), col = "White") +
+    scale_fill_grey(start=0,end=0.9) +
+    ylab(ylab) +
+    xlab(xlab) +
+    scale_y_continuous(labels = scales::percent) +
+    scale_x_continuous(labels=NULL) +
+    geom_text(aes(label = as.character(var1), x = var1Center, y = -0.05, size=0.5)) +
+    guides(fill="none", size="none") +
+    coord_fixed(ratio=1) +
+    theme_pander()
+}
+
+
+t1.trend <- function(v=var, strata, data=wdt, var.cont=FALSE) {
+    '
+    Report if there is a trend across categories (strata)
+    Assumes that var is categorical (then Cochran-Armitage) unless var.cont=TRUE
+    '
+    require(data.table)
+    require(assertthat)
+    if (var.cont) {
+        m <- lm(get(v)~ as.numeric(get(strata)), data=data)
+        return(summary(m)$coefficients[2,4])
+    } else {
+        t.n <- table(data[!is.na(get(v)), strata, with=FALSE])
+        t.0 <- table(data[!is.na(get(v)) & get(v) == 0, strata, with = FALSE])
+        t.1 <- table(data[!is.na(get(v)) & get(v) == 1, strata, with = FALSE])
+        # lapply(list(v, t.n, t.0, t.1), function(x) print(x)) # PRINT FOR DEBUGGING
+        # Check the variable is binary
+        assert_that(sum(as.vector(t.0) + as.vector(t.1) - as.vector(t.n))==0)
+        return(prop.trend.test(t.1,t.n)$p.value)
+    }
+}
+
 # intra-class correlation
 ICC <- function(m) {
     require(lme4)
@@ -19,7 +70,7 @@ ICC <- function(m) {
 }
 
 # function to produce coefficient plots
-coef.plot <- function(model=m) {
+coef.plot <- function(model=m, m.labels=NULL) {
 
     # Load packages
     require(data.table)
@@ -38,6 +89,9 @@ coef.plot <- function(model=m) {
         colnames(m.coef)[1] <- "est"
         colnames(m.coef)[2] <- "se"
         colnames(m.coef)[3] <- "t"
+        # - [ ] FIXME(2016-03-18): Cheating and using the normal approx to get p values
+        m.coef$z <- m.coef$est / m.coef$se
+        m.coef$p <- pnorm(abs(m.coef$z), lower.tail=FALSE)
     } else if (class(m)[1]=="glm") {
         # If converting then will need to know link function
         m.coef <- data.frame(summary(m)$coefficients)
@@ -61,15 +115,30 @@ coef.plot <- function(model=m) {
     m.coef <- data.table(m.coef, keep.rownames=TRUE)
     # Remove the intercept
     m.coef <- m.coef[rn!="(Intercept)"]
+    # Force ordering of indep vars
+    m.coef[, ivar := factor(.I, labels=m.coef$rn, ordered=TRUE)]
     print(m.coef)
+    length(m.coef$rn)
 
-    gg.p <- ggplot(data=m.coef, aes(x=rn, y=est)) + 
+    if (is.null(m.labels)) {
+        m.labels <- rev(m.coef$rn)
+    } else {
+        assert_that(length(m.labels)==length(m.coef$rn))
+        # Check they line up
+        print(data.frame(m.labels, m.coef$rn))
+        # Now reverse so ordered top to bottom in plot
+        m.labels <- rev(m.labels)
+    }
+
+    gg.p <- ggplot(data=m.coef, aes(x=ivar, y=est)) + 
         geom_hline(yintercept=0, colour="grey", ) +
         geom_linerange(aes(ymin=est - 2 * se, ymax=est + 2 * se), colour="grey") +
         geom_pointrange(aes(ymin=est - 1 * se, ymax=est + 1 * se), lwd=0.5, fatten=2) +
+        scale_x_discrete(limits=rev(levels(m.coef$ivar)), labels=m.labels) +
         xlab("Predictor") + ylab("Effect estimate") +
         coord_flip() +
         theme_minimal()
+    # print(gg.p)
     return(gg.p)
 }
 
