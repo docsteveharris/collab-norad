62bfe350d6142a6d6b6f9577dfe9ce5ea7f24b67
Steve Harris (docsteveharris) <git@steveharris.me>
2016-03-25 22:12:46 +0000

    added mosaic plots to process variable figures

--- analysis/fig_process_var.R
+++ analysis/fig_process_var.R
@@ -86,3 +86,87 @@ ggsave(p, file="../write/figures/process_fb24.eps", width=3, height=3, scale=2)
 # 	theme_minimal()
 	
 
+# Mosaic plot with ggplot function
+ggMMplot <- function(var1, var2, ylab=""){
+  require(ggplot2)
+  levVar1 <- length(levels(var1))
+  levVar2 <- length(levels(var2))
+
+  jointTable <- prop.table(table(var1, var2))
+  plotData <- as.data.frame(jointTable)
+  plotData$marginVar1 <- prop.table(table(var1))
+  plotData$var2Height <- plotData$Freq / plotData$marginVar1
+  plotData$var1Center <- c(0, cumsum(plotData$marginVar1)[1:levVar1 -1]) +
+    plotData$marginVar1 / 2
+
+  ggplot(plotData, aes(var1Center, var2Height)) +
+    geom_bar(stat = "identity", aes(width = marginVar1, fill = var2), col = "White") +
+    scale_fill_grey(start=0,end=0.9) +
+    ylab(ylab) +
+    xlab("Intensive Care Unit") +
+    scale_y_continuous(labels = scales::percent) +
+    scale_x_continuous(labels=NULL) +
+    geom_text(aes(label = as.character(var1), x = var1Center, y = -0.05, size=0.5)) +
+    guides(fill="none", size="none") +
+    coord_fixed(ratio=1) +
+    theme_pander()
+}
+
+# Sedation
+summary(wdt$sedation.24)
+tdt <- na.omit(
+           wdt[, .(
+               hosp.id.sort,
+            sedation.24.logical = ifelse(sedation.24 > 0, 1, 0)
+            )])
+p <- ggMMplot(tdt$hosp.id.sort, !tdt$sedation.24.logical, ylab="Sedation")
+p
+
+
+# VADI
+# - [ ] TODO(2016-03-24): remove norad from VADI score or calc by hand
+summary(wdt$vadi.24)
+summary(wdt$ne.24)
+# Hand calculate vadi without norad to estimate use of other vasopressors
+tdt <- na.omit(
+           wdt[, .(
+                hosp.id.sort,
+                vadi.24,
+                ne.24,
+                vadi.other.24 = 
+                    ifelse(ne.24 > 0.3, vadi.24 - 3,
+                    ifelse(ne.24 > 0.1, vadi.24 - 2,
+                    ifelse(ne.24 > 0.0, vadi.24 - 1, vadi.24))))])
+tdt[, vadi.other.24.logical := ifelse(vadi.other.24 > 0, 1, 0)]
+p <- ggMMplot(tdt$hosp.id.sort, tdt$vadi.other.24.logical, ylab="Other vasopressors/inotropes")
+p
+
+# Steroids
+summary(wdt$rx.roids)
+tdt <- na.omit(
+           wdt[, .(
+                hosp.id.sort,
+                rx.roids.logical = ifelse(rx.roids==TRUE, 1, 0)
+            )])
+p <- ggMMplot(tdt$hosp.id.sort, tdt$rx.roids.logical, ylab="Corticosteroids")
+p
+
+# Mechanical ventilation
+summary(wdt$mv.24)
+tdt <- na.omit(
+           wdt[, .(
+                hosp.id.sort,
+                mv.24
+            )])
+p <- ggMMplot(tdt$hosp.id.sort, !tdt$mv.24, ylab="Mechanical ventilation")
+p
+
+# Renal replacement therapy
+summary(wdt$rrt.24)
+tdt <- na.omit(
+           wdt[, .(
+                hosp.id.sort,
+                rrt.24
+            )])
+p <- ggMMplot(tdt$hosp.id.sort, !tdt$rrt.24, ylab="Renal replacement therapy")
+p
