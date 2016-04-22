b0848ec8cfa2c20b5332ddb20753a24ce2c8afe0
drstevok <git@steveharris.me>
2016-04-22 21:03:36 +0100

    moved ggMMplot to functions file

--- analysis/model_subgrp.R
+++ analysis/model_subgrp.R
@@ -30,62 +30,14 @@ source(file="../share/functions4paper.R")
 wdt[,.(mort=mean(mort.itu),.N),by=.(hosp.id.sort, hosp)]
 with(wdt, table(hosp.id.sort))
 
-# Mosaic plot with ggplot function
-ggMMplot <- function(var1, var2, palette="YlOrRd"){
-  require(ggplot2)
-
-  # Developing
-  # var1 <- wdt$hosp.id.sort
-  # var2 <- wdt$grp
-
-  levVar1 <- length(levels(var1))
-  levVar2 <- length(levels(var2))
-
-  jointTable <- prop.table(table(var1, var2))
-  plotData <- as.data.frame(jointTable)
-  plotData$marginVar1 <- prop.table(table(var1))
-  plotData$marginVar2 <- prop.table(table(var2))
-  plotData$var2Height <- plotData$Freq / plotData$marginVar1
-  plotData$var1Center <- c(0, cumsum(plotData$marginVar1)[1:levVar1 -1]) +
-    plotData$marginVar1 / 2
-
-  # Define label positions on LEFT (y-axis)
-  ylabData <- plotData[plotData$var1==levels(plotData$var1)[1],]
-  dd <- (y=c(0, cumsum(ylabData$var2Height)))
-  ylabData$ylabCenter <- sapply(1:(length(dd)-1), function(x) dd[x] + (dd[x+1] - dd[x])/2 )
-  print(ylabData)
-
-  # Define label positions on the BOTTOM (x-axis)
-  xlabData <- plotData[plotData$var2==levels(plotData$var2)[1],]
-  dd <- (x=c(0, cumsum(xlabData$marginVar1)))
-  xlabData$xlabCenter <- sapply(1:(length(dd)-1), function(x) dd[x] + (dd[x+1] - dd[x])/2 )
-  print(xlabData)
-
-  ggplot(plotData, aes(var1Center, var2Height)) +
-    geom_bar(stat = "identity", aes(width = marginVar1, fill = var2), col = "White") +
-    scale_fill_brewer(type="seq", palette=palette, guide=FALSE) +
-    # xlabels
-    geom_text(data=xlabData,
-              aes(label = as.character(var1), x = xlabCenter, y = -0.05),
-              vjust="inward") +
-    # ylabels
-    geom_text(data=ylabData, 
-              aes(label = as.character(var2), y = ylabCenter, x = -0.05),
-                  vjust="top", angle=90) +
-    xlab("") + scale_x_discrete(labels=NULL) +
-    ylab("") + scale_y_discrete(labels=NULL) +
-    theme_minimal() +
-    theme(plot.margin=margin(rep(20,4)))
-}
 
 
 nrow(wdt)
 wdt[, grp := 1]
 wdt[, grp := ifelse(grp.ne24 == 1, 2, grp)]
 wdt[, grp := ifelse(grp.morelli == 1, 3, grp)]
-wdt[, grp := factor(grp, levels=c(1,2,3), labels=c("tachycardic-shock", "resistant-shock", "shock"), ordered=TRUE)]
+wdt[, grp := factor(grp, levels=c(3,2,1), labels=c("shock", "resistant-shock", "tachycardic-shock"), ordered=TRUE)]
 with(wdt, table(hosp.id.sort, grp))
-
 
 gg_p <- with(wdt, ggMMplot(hosp.id.sort, grp, palette="Greys"))
 gg_p <- gg_p + xlab("ICU") + ylab("Shock subgroup")
--- share/functions4paper.R
+++ share/functions4paper.R
@@ -2,31 +2,56 @@
 
 # Mosaic plot with ggplot function
 # Mosaic plot with ggplot function
-ggMMplot <- function(var1, var2, xlab="", ylab=""){
+# Mosaic plot with ggplot function
+ggMMplot <- function(var1, var2, palette="YlOrRd"){
   require(ggplot2)
-  require(ggthemes)
+
+  # Developing
+  # var1 <- wdt$hosp.id.sort
+  # var2 <- wdt$grp
+
   levVar1 <- length(levels(var1))
   levVar2 <- length(levels(var2))
 
   jointTable <- prop.table(table(var1, var2))
   plotData <- as.data.frame(jointTable)
   plotData$marginVar1 <- prop.table(table(var1))
+  plotData$marginVar2 <- prop.table(table(var2))
   plotData$var2Height <- plotData$Freq / plotData$marginVar1
   plotData$var1Center <- c(0, cumsum(plotData$marginVar1)[1:levVar1 -1]) +
     plotData$marginVar1 / 2
 
+  # Define label positions on LEFT (y-axis)
+  ylabData <- plotData[plotData$var1==levels(plotData$var1)[1],]
+  dd <- (y=c(0, cumsum(ylabData$var2Height)))
+  ylabData$ylabCenter <- sapply(1:(length(dd)-1), function(x) dd[x] + (dd[x+1] - dd[x])/2 )
+  print(ylabData)
+
+  # Define label positions on the BOTTOM (x-axis)
+  xlabData <- plotData[plotData$var2==levels(plotData$var2)[1],]
+  dd <- (x=c(0, cumsum(xlabData$marginVar1)))
+  xlabData$xlabCenter <- sapply(1:(length(dd)-1), function(x) dd[x] + (dd[x+1] - dd[x])/2 )
+  print(xlabData)
+
   ggplot(plotData, aes(var1Center, var2Height)) +
     geom_bar(stat = "identity", aes(width = marginVar1, fill = var2), col = "White") +
-    scale_fill_grey(start=0,end=0.9) +
-    ylab(ylab) +
-    xlab(xlab) +
-    scale_y_continuous(labels = scales::percent) +
-    scale_x_continuous(labels=NULL) +
-    geom_text(aes(label = as.character(var1), x = var1Center, y = -0.05, size=0.5)) +
+    scale_fill_brewer(type="seq", palette=palette) +
+    # xlabels
+    geom_text(data=xlabData,
+              aes(label = as.character(var1), x = xlabCenter, y = -0.05),
+              vjust="inward") +
+    # ylabels
+    geom_text(data=ylabData, 
+              aes(label = as.character(var2), y = ylabCenter, x = -0.05),
+                  vjust="top", angle=90) +
+    xlab("") + scale_x_discrete(labels=NULL) +
+    ylab("") + scale_y_discrete(labels=NULL) +
     guides(fill="none", size="none") +
     coord_fixed(ratio=1) +
-    theme_pander()
+    theme_pander() +
+    theme(plot.margin=margin(rep(20,4)))
 }
+
 
 
 t1.trend <- function(v=var, strata, data=wdt, var.cont=FALSE) {
